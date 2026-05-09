c94546d360228375d4f90fbd119754d821ea0b76
Estelior <ll97@mail.ru>
2026-05-09 17:50:53 +0300

    Allure2

--- qaGuru-Allure/src/test/java/LambdaTest.java
+++ qaGuru-Allure/src/test/java/LambdaTest.java
@@ -1,7 +1,9 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Step;
+import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import testData.TestData;
 
@@ -11,9 +13,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 public class LambdaTest {
-
     TestData testData = new TestData();
     @Test
+    @Feature("Issue в репозитории")
+    @Story("Создание Issue")
+    @Severity(SeverityLevel.BLOCKER)
+    @DisplayName("Тест с Lambda")
+    @Tag("test")
+    @Owner("LazVal")
     public void testIssueSearchLambda() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
@@ -22,23 +29,21 @@ public class LambdaTest {
         });
 
         step("Нажимаем на поиск", () -> {
-
             $x("//button[@data-action='click:qbsearch-input#handleExpand']").click();
         });
 
         step("Ищем репозиторий", () -> {
-
             $x("//input[@id='query-builder-test']").sendKeys(testData.repository);
             $x("//input[@id='query-builder-test']").submit();
         });
 
         step("Нажимаем на Issues", () -> {
-
             $x("//a[@href='/LazVal/qaGuru-Allure']").click();
             $("#issues-tab").click();
         });
 
-        step("Проверка наличия Issue");
-        $(withText("TestIssues")).should(Condition.exist);
+        step("Проверка наличия Issue", () -> {
+            $(withText("TestIssues")).should(Condition.exist);
+        });
     }
 }
--- qaGuru-Allure/src/test/java/SelenideTest.java
+++ qaGuru-Allure/src/test/java/SelenideTest.java
@@ -1,6 +1,9 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -9,8 +12,13 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.linkText;
 
 public class SelenideTest {
-
     @Test
+    @Feature("Issue в репозитории")
+    @Story("Создание Issue")
+    @Severity(SeverityLevel.BLOCKER)
+    @DisplayName("Тест с Listener")
+    @Tag("test")
+    @Owner("LazVal")
     public void testIssueSearch() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
